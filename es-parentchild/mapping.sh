f3ad9bfd14a62f3136f0588638f1e5867ebdd70b
arunpmohan_fw <arunmohan@factweavers.com>
2020-01-18 15:10:13 +0530

    parent-child documents

--- es-parentchild/mapping.sh
+++ es-parentchild/mapping.sh
@@ -1,4 +1,19 @@
-curl -X PUT "localhost:9200/test-index" -H 'Content-Type: application/json' -d'
+read -p "Enter ES host : " host
+read -p "Enter ES port : " port
+read -p "username for elastic, if any?" username
+read -p "password for the user" password
+echo "entered host URL is $host:$port"
+url=$host:$port
+
+if [ (-z $username ) -a (-z $password ) ]
+then
+	echo "OK"
+else
+	echo "Not OK"
+fi
+
+echo "$url"
+curl -X PUT "$url/test-index-parent-child" -H 'Content-Type: application/json' -d'
 {
   "mappings": {
     "_doc": {
